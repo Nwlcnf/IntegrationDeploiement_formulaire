1a383e0907090b31a93cb5f96ee66e40c13bc17a
Nwlcnf <Nwlcnf@users.noreply.github.com>
2024-10-31 16:29:21 +0000

    deploy: 89bafb62bc827bb9f827967ec2e6d9aa71478c9c

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders form title', () => {
-    render(<App />);
-    const titleElement = screen.getByText(/Formulaire d'inscription/i);
-    expect(titleElement).toBeInTheDocument();
-});
